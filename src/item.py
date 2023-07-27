d21d176d610d7e5cb733139e9ba01debe13a68bc
AlbertTesco <artem-dedov2012@yandex.ru>
2023-07-28 00:31:34 +0300

    Adding functionality to the Item class. Added tests

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +16,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
@@ -30,4 +33,36 @@ class Item:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        self.price = self.price * Item.pay_rate
+        self.price *= Item.pay_rate
+
+    @property
+    def name(self) -> str:
+        """
+        Getter для имени товара в магазине.
+        """
+        return self.__name
+
+    @name.setter
+    def name(self, value: str) -> None:
+        """
+        Setter для имени товара в магазине.
+        """
+        if len(value) > 10:
+            self.__name = value[:10]
+        else:
+            self.__name = value
+
+    @classmethod
+    def instantiate_from_csv(cls):
+
+        try:
+            with open('../src/items.csv', 'r', encoding='utf-8') as f:
+                csv_dict = csv.DictReader(f)
+                for row in csv_dict:
+                    Item(row["name"], cls.string_to_number(row['price']), int(row['quantity']))
+        except FileNotFoundError:
+            print("Файл items.csv не найден")
+
+    @staticmethod
+    def string_to_number(string: str) -> float:
+        return int(float(string))
